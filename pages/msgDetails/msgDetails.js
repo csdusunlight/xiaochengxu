5b6441d55afb1e278b3a7cf686c5c7c99991f34c
llc <763008201@qq.com>
2018-03-15 16:46:46 +0800

    llc 账户绑定页面 通知公告页面 交单记录

--- pages/msgDetails/msgDetails.js
+++ pages/msgDetails/msgDetails.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-      array:[]
+    array: []
   },
 
   /**
@@ -15,72 +15,80 @@ Page({
   onLoad: function (options) {
     let that = this;
     var token = wx.getStorageSync("token");
-        wx.request({
-          url: 'http://test.51fanshu.com/restapi/notice',
-          data: {},
-          header: {
-            'app-id': app.globalData.app_id,
-            'AUTHORIZATION': token
-          },
-          method: 'GET',
-          dataType: 'json',
-          responseType: 'text',
-          success: function(res) {
-            console.log("----", res.data.results);
-              let dataList = res.data.results;
-              that.setData({
-                array:dataList
-              })
-          }
+    // var token = "";
+    //判断是否能获取到用户信息，如果不能获取到用户信息，把array设置为空，如果能获取到用户信息，则更新array
+    if (!app.globalData.userInfo.nickName){
+        that.setData({
+          array:''
         })
+    }else{
+      wx.request({
+        url: 'http://test.51fanshu.com/restapi/notice',
+        data: {},
+        header: {
+          'app-id': app.globalData.app_id,
+          'AUTHORIZATION': token
+        },
+        method: 'GET',
+        dataType: 'json',
+        responseType: 'text',
+        success: function (res) {
+          console.log("----", res.data.results);
+          let dataList = res.data.results;
+          that.setData({
+            array: dataList
+          })
+        }
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
--- pages/orderRecord/orderRecord.js
+++ pages/orderRecord/orderRecord.js
@@ -1,12 +1,13 @@
 var app = getApp();
 var pages = 1;     //设置分页
-var pageSize = 10; //加载数据的条数
+var pageSize = 12; //加载数据的条数
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    array: []
+    array: [],
+    xianshi: false
   },
 
   /**
@@ -14,68 +15,45 @@ Page({
    */
   onLoad: function (options) {
     var that = this;
-    var token = wx.getStorageSync('token');
-    // var token = "";
-    wx.request({
-      url: 'http://test.51fanshu.com/xcx/investlogs/?page='+pages+'&pageSize='+pageSize,
-      method: "get",
-      header: {
-        'app-id': app.globalData.app_id,
-        'AUTHORIZATION': token
-      },
-      success: (res) => {
-        let dataList = res.data.results;
-        console.log("---dataList---:",dataList);
-        dataList.forEach((item) => {
-          item.submit_time = item.submit_time.substring(0, 10);
-        })
-        that.setData({
-          array: dataList,
-        })
-      },
-      fail:function(){
-        console.log(error);
-      }
-    })
+    that.loadMore()
+
   },
   //删除数据
   delete: function (e) {
+    console.log(e);
+
     let that = this;
-    var token = wx.getStorageSync('token');
+    var token = wx.getStorageSync('token');//获取token
     var dId = e.currentTarget.id;//获取当前数据的id
-    console.log(dId);
+    // console.log("-- did --",dId);
     wx.showModal({
       title: '提示',
       content: '是否要删除该条数据？',
       success: function (event) {
         if (event.confirm) {
-          console.log("删除该条数据")
           wx.request({
-            url: 'http://test.51fanshu.com/xcx/investlogs/',
+            url: 'http://test.51fanshu.com/xcx/investlogs/' + dId,
             method: "delete",
             data: {
-              id:dId
+              // id:dId
             },
             header: {
               'app-id': app.globalData.app_id,
               'AUTHORIZATION': token
             },
-            success: function () {
-                wx.showToast({
-                  title: '删除成功！',
-                })
+            success: function (e) {
+              wx.showToast({
+                title: '删除成功！',
+              })
+              console.error(e)
             },
           })
         } else if (event.cancel) {
           console.log("取消");
         }
-        //更新列表状态
-        let list = that.data.dataList;
-        that.setData({
-            array:list
-        })
+
       }
-    
+
     })
   },
   /**
@@ -106,16 +84,55 @@ Page({
   },
 
   /**
-   * 页面相关事件处理函数--监听用户下拉动作
+   * 页面相关事件处理函数--监听用户下拉动作   
    */
   onPullDownRefresh: function () {
-    wx.stopPullDownRefresh()
+
+  },
+  loadMore: function () {
+    var that = this;
+    var token = wx.getStorageSync('token');
+    //判断用户是否登录，没有登陆，设置array没空；登陆了则进行request请求
+    if (!app.globalData.userInfo.nickName) {
+      that.setData({
+        array: ""
+      })
+    } else {
+      wx.request({
+        url: 'http://test.51fanshu.com/xcx/investlogs/?page=' + pages + '&pageSize=' + pageSize,
+        method: "get",
+        header: {
+          'app-id': app.globalData.app_id,
+          'AUTHORIZATION': token
+        },
+        success: (res) => {
+          let dataList = res.data.results;
+          console.log("---dataList---:",dataList);
+          dataList.forEach((item) => {
+            item.submit_time = item.submit_time.substring(0, 10);
+          })
+          that.setData({
+            array: dataList,
+          })
+          // console.log(that.data.array)自己做处理吧  
+        },
+        fail: function () {
+          console.log(error);
+        }
+      })
+    }
   },
 
   /**
-   * 页面上拉触底事件的处理函数
+   * 页面上拉触asd底事件的处理函数
    */
   onReachBottom: function () {
+    wx.showNavigationBarLoading();
+    this.loadMore();
+    wx.hideNavigationBarLoading()
+
+    var token = wx.getStorageSync('token');
+    var that = this;
     console.log("下拉loading~");
     wx.request({
       url: 'http://test.51fanshu.com/xcx/investlogs/?page=' + pages + '&pageSize=' + pageSize,
@@ -124,15 +141,29 @@ Page({
         'app-id': app.globalData.app_id,
         'AUTHORIZATION': token
       },
-      success:(res) =>{
+      success: (res) => {//res请求成功并返回的数据
         let dataList = res.data.results;
-        dataList.forEach((item) => {
-          item.submit_time = item.submit_time.substring(0,10);
-        });
-        pages++;
-        that.setData({
-          array:dataList
-        })
+        if (res.data.results) {
+          //判断data是否有值   有循环并push 没有直接让xianshi为true
+          dataList.forEach((item) => {
+            item.submit_time = item.submit_time.substring(0, 10);
+          });
+          for (var i = 0, l = dataList.length; i < l; i++) {
+            that.data.array.push(dataList[i])
+          }
+          pages++;
+          that.setData({
+            array: that.data.array
+          })
+        } else {
+          that.setData({
+            xianshi: true
+          })
+        }
+      },
+      fail: function () {
+        console.error(11111);
+
       }
     })
   },
