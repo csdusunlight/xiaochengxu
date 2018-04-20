8f67fd48b2589144d57d6a584e691e9ea4d1e118
llc <763008201@qq.com>
2018-04-20 14:04:12 +0800

    llc 小程序增加页面

---
+++ data/data.js
@@ -0,0 +1,38 @@
+var listData = {
+  "results": [
+    {
+      "id": 1,
+      "title": "标题一",
+      "description": "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+      "imgSrc": "../../images/news/news1.jpg"
+    },
+    {
+      "id": 2,
+      "title": "标题二",
+      "description": "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+      "imgSrc": "../../images/news/news2.jpg"
+    },
+    {
+      "id": 3,
+      "title": "标题三",
+      "description": "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+      "imgSrc": "../../images/news/news3.jpg"
+    },
+    {
+      "id": 4,
+      "title": "标题四",
+      "description": "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。", 
+      "imgSrc": "../../images/news/news4.jpg"
+    },
+    {
+      "id": 5,
+      "title": "标题五",
+      "description": "由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。",
+      "imgSrc": "../../images/news/news5.jpg"
+    },
+  ]
+}
+
+module.exports = {
+  listData: listData
+}
