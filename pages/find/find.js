c061d85ea47e1a1033497b4c505fa8afdb089909
llc <763008201@qq.com>
2018-05-07 17:08:41 +0800

    llc 小程序发现页面重写

--- pages/find/find.js
+++ pages/find/find.js
@@ -8,68 +8,94 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    // 页面配置 
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换  
+    currentTab: 0,
+    //数据
+    data:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
     wx.setNavigationBarTitle({
       title: app.globalData.app_name
     })
-    var findData = dataList.listData.results;
-    this.setData({
-      findData: findData
+    console.log(dataList.listData.results);
+    that.setData({
+      data: dataList.listData.results
     })
   },
-
+  // tab点击切换
+  swichNav: function (e) {
+    console.log(e.target.dataset.current)
+    var that = this;
+    if (this.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      });
+    }
+  },
+  //tab点击切换到当前页面
+  bindChange: function (e) {
+    var that = this;
+    that.setData({
+       currentTab: e.detail.current
+    });
+  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
